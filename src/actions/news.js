7c9d7bee64f1019ef1bf6474e4baf4bbc40f9dab
Ivan Harasta <harasta.ivan@gmail.com>
2019-10-21 21:00:29 +0200

    Remove proxy, add API_ENDPOINT to .env and change error handling

--- src/actions/news.js
+++ src/actions/news.js
@@ -3,41 +3,43 @@ import axios from 'axios';
 import { tokenConfig } from './auth';
 import { returnErrors } from './error';
 
-export const getNews = () => async (dispatch) => {
-	dispatch(setNewsLoading);
-	const res = await axios.get('/api/news');
-	dispatch({
-		type: GET_NEWS,
-		payload: res.data
-	});
+const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
+
+export const getNews = () => async dispatch => {
+    dispatch(setNewsLoading);
+    const res = await axios.get(`${API_ENDPOINT}/api/news`);
+    dispatch({
+        type: GET_NEWS,
+        payload: res.data
+    });
 };
 
-export const addNews = (news) => async (dispatch, getState) => {
-	try {
-		const res = await axios.post('/api/news', news, tokenConfig(getState));
-		dispatch({
-			type: ADD_NEWS,
-			payload: res.data
-		});
-	} catch (err) {
-		dispatch(returnErrors(err.response.data, err.response.status));
-	}
+export const addNews = news => async (dispatch, getState) => {
+    try {
+        const res = await axios.post(`${API_ENDPOINT}/api/news`, news, tokenConfig(getState));
+        dispatch({
+            type: ADD_NEWS,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch(returnErrors(err));
+    }
 };
 
-export const deleteNews = (id) => async (dispatch, getState) => {
-	try {
-		await axios.delete(`/api/news/${id}`, tokenConfig(getState));
-		dispatch({
-			type: DELETE_NEWS,
-			payload: id
-		});
-	} catch (err) {
-		dispatch(returnErrors(err.response.data, err.response.status));
-	}
+export const deleteNews = id => async (dispatch, getState) => {
+    try {
+        await axios.delete(`${API_ENDPOINT}/api/news/${id}`, tokenConfig(getState));
+        dispatch({
+            type: DELETE_NEWS,
+            payload: id
+        });
+    } catch (err) {
+        dispatch(returnErrors(err));
+    }
 };
 
 export const setNewsLoading = () => {
-	return {
-		type: NEWS_LOADING
-	};
+    return {
+        type: NEWS_LOADING
+    };
 };
