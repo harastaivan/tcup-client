78cf61bb7defc70e65fde8a9796258f527611965
Ivan Harasta <harasta.ivan@gmail.com>
2019-10-24 00:14:04 +0200

    Add useful Alerts

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import RegistrationForm from './RegistrationForm';
+import EditRegistrationForm from './EditRegistrationForm';
+import Login from './Login';
+import { Alert } from 'reactstrap';
 
-export default class Registration extends Component {
-	render() {
-		return <div>Přihláška</div>;
-	}
+class Registration extends Component {
+    render() {
+        return (
+            <div>
+                {!this.props.isAuthenticated && (
+                    <div>
+                        <Alert color='info'>Pro vytvoření přihlášky se přihlaste.</Alert>
+                        <Login />
+                    </div>
+                )}
+                {this.props.isAuthenticated && this.props.registration && this.props.registration.isRegistered && (
+                    <EditRegistrationForm disabled={true} />
+                )}
+                {this.props.isAuthenticated && !(this.props.registration && this.props.registration.isRegistered) && (
+                    <RegistrationForm />
+                )}
+            </div>
+        );
+    }
 }
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    registration: state.registration
+});
+
+const mapDispatchToProps = {};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Registration);
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,156 +1,144 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {
-	Alert,
-	Form,
-	FormGroup,
-	Label,
-	Input,
-	Button,
-	Row,
-	Col
-} from 'reactstrap';
+import { Alert, Form, FormGroup, Label, Input, Button, Row, Col } from 'reactstrap';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { register } from '../actions/auth';
 import { clearErrors } from '../actions/error';
 
 class Signup extends Component {
-	state = {
-		name: '',
-		surname: '',
-		email: '',
-		password: '',
-		msg: null
-	};
+    state = {
+        name: '',
+        surname: '',
+        email: '',
+        password: '',
+        msg: null
+    };
 
-	static propTypes = {
-		isAuthenticated: PropTypes.bool,
-		error: PropTypes.object.isRequired,
-		register: PropTypes.func.isRequired,
-		clearErrors: PropTypes.func.isRequired
-	};
+    static propTypes = {
+        isAuthenticated: PropTypes.bool,
+        error: PropTypes.object.isRequired,
+        register: PropTypes.func.isRequired,
+        clearErrors: PropTypes.func.isRequired
+    };
 
-	onChange = e => {
-		this.setState({
-			[e.target.name]: e.target.value
-		});
-	};
+    onChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
 
-	onSubmit = e => {
-		e.preventDefault();
-		const { name, surname, email, password } = this.state;
+    onSubmit = e => {
+        e.preventDefault();
+        const { name, surname, email, password } = this.state;
 
-		const newUser = {
-			name,
-			surname,
-			email,
-			password
-		};
-		this.props.register(newUser);
-		this.setState({
-			password: ''
-		});
-	};
+        const newUser = {
+            name,
+            surname,
+            email,
+            password
+        };
+        this.props.register(newUser);
+        this.setState({
+            password: ''
+        });
+    };
 
-	componentDidUpdate(previousProps) {
-		const { error, isAuthenticated } = this.props;
-		if (error !== previousProps.error) {
-			// Check for register error
-			if (error.id === 'REGISTER_FAIL') {
-				this.setState({ msg: error.msg.msg });
-			} else {
-				this.setState({ msg: null });
-			}
-		}
-		if (isAuthenticated) {
-			this.props.clearErrors();
-			this.props.history.push('/');
-		}
-	}
+    componentDidUpdate(previousProps) {
+        const { error, isAuthenticated } = this.props;
+        if (error !== previousProps.error) {
+            // Check for register error
+            if (error.id === 'REGISTER_FAIL') {
+                this.setState({ msg: error.msg.msg });
+            } else {
+                this.setState({ msg: null });
+            }
+        }
+        if (isAuthenticated) {
+            this.props.clearErrors();
+            this.props.history.push('/');
+        }
+    }
 
-	render() {
-		return (
-			<div>
-				{this.state.msg ? (
-					<Alert color='danger'>{this.state.msg}</Alert>
-				) : null}
-				<h1>Registrovat se</h1>
-				<Form onSubmit={this.onSubmit} autoComplete={'off'}>
-					<Row form>
-						<Col md={6}>
-							<FormGroup>
-								<Label for='name'>Jméno</Label>
-								<Input
-									type='text'
-									name='name'
-									id='name'
-									placeholder='Jméno'
-									value={this.state.name}
-									onChange={this.onChange}
-								/>
-							</FormGroup>
-						</Col>
-						<Col md={6}>
-							<FormGroup>
-								<Label for='surname'>Příjmení</Label>
-								<Input
-									type='text'
-									name='surname'
-									id='surname'
-									placeholder='Příjmení'
-									value={this.state.surname}
-									onChange={this.onChange}
-								/>
-							</FormGroup>
-						</Col>
-					</Row>
-					<FormGroup>
-						<Label for='email'>Email</Label>
-						<Input
-							type='email'
-							name='email'
-							id='email'
-							placeholder='Email'
-							value={this.state.email}
-							onChange={this.onChange}
-						/>
-					</FormGroup>
-					<FormGroup>
-						<Label for='password'>Heslo</Label>
-						<Input
-							type='password'
-							name='password'
-							id='password'
-							placeholder='Heslo'
-							value={this.state.password}
-							onChange={this.onChange}
-						/>
-					</FormGroup>
-					<Button
-						color='dark'
-						style={{ marginTop: '2rem' }}
-						disabled={
-							!this.state.name ||
-							!this.state.surname ||
-							!this.state.email ||
-							!this.state.password
-						}
-						block
-					>
-						Registrovat se
-					</Button>
-				</Form>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div>
+                {this.state.msg ? <Alert color='danger'>{this.state.msg}</Alert> : null}
+                <h1>Registrovat se</h1>
+                <Form onSubmit={this.onSubmit} autoComplete={'off'}>
+                    <Row form>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for='name'>Jméno</Label>
+                                <Input
+                                    type='text'
+                                    name='name'
+                                    id='name'
+                                    placeholder='Jméno'
+                                    value={this.state.name}
+                                    onChange={this.onChange}
+                                />
+                            </FormGroup>
+                        </Col>
+                        <Col md={6}>
+                            <FormGroup>
+                                <Label for='surname'>Příjmení</Label>
+                                <Input
+                                    type='text'
+                                    name='surname'
+                                    id='surname'
+                                    placeholder='Příjmení'
+                                    value={this.state.surname}
+                                    onChange={this.onChange}
+                                />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                    <FormGroup>
+                        <Label for='email'>Email</Label>
+                        <Input
+                            type='email'
+                            name='email'
+                            id='email'
+                            placeholder='Email'
+                            value={this.state.email}
+                            onChange={this.onChange}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for='password'>Heslo</Label>
+                        <Input
+                            type='password'
+                            name='password'
+                            id='password'
+                            placeholder='Heslo'
+                            value={this.state.password}
+                            onChange={this.onChange}
+                        />
+                    </FormGroup>
+                    <Button
+                        color='dark'
+                        style={{ marginTop: '2rem' }}
+                        disabled={!this.state.name || !this.state.surname || !this.state.email || !this.state.password}
+                        block
+                    >
+                        Registrovat se
+                    </Button>
+                </Form>
+                <Alert color='light' style={{ marginTop: '2rem' }}>
+                    Již máte účet? <Link to='/login'>Přihlaste se.</Link>
+                </Alert>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => ({
-	isAuthenticated: state.auth.isAuthenticated,
-	error: state.error
+    isAuthenticated: state.auth.isAuthenticated,
+    error: state.error
 });
 
 export default connect(
-	mapStateToProps,
-	{ register, clearErrors }
+    mapStateToProps,
+    { register, clearErrors }
 )(Signup);
