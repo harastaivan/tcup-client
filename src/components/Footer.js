b5af6f44cc3151b7f8445023d2a3f8fb22f78aea
Ivan Harasta <harasta.ivan@gmail.com>
2019-11-19 23:27:25 +0100

    Fix landing page, img was fucked hard

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,21 +4,16 @@ import { Container } from 'reactstrap';
 
 class Footer extends Component {
 	render() {
-		const isHomePage = this.props.history.location.pathname === '/';
+		// const isHomePage = this.props.history.location.pathname === '/';
 		return (
 			<Container className='transparent-background'>
-				<footer className={`p-5 py-4 text-center text-small ${isHomePage && 'text-white'}`}>
+				<footer className={`p-5 py-4 text-center text-small`}>
 					<p>
 						© {process.env.REACT_APP_TITLE} <b>verze {process.env.REACT_APP_VERSION}</b>
 					</p>
 					<p>
 						by{' '}
-						<a
-							target='_blank'
-							rel='noopener noreferrer'
-							href='https://github.com/harastaivan'
-							className={`${isHomePage ? 'text-light-blue' : ''}`}
-						>
+						<a target='_blank' rel='noopener noreferrer' href='https://github.com/harastaivan'>
 							@harastaivan
 						</a>
 					</p>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,9 @@ class Home extends Component {
 	render() {
 		const newsMessage = { before: 'Dnes nebyly publikovány ', in: 'žádné novinky', end: '.' };
 		return (
-			<div className='text-white'>
+			<div>
 				<h1>{process.env.REACT_APP_TITLE}</h1>
-				<img src='/img/homepage.jpg' alt='Společná fotka soutěžících' className='homepage'></img>
+				{/*<img src='/img/homepage.jpg' alt='Společná fotka soutěžících' className='homepage'></img>*/}
 				<h4 className='mt-5 mb-5'>Letošní tcup se bude konat {process.env.REACT_APP_FROM_TO} v Toužimi.</h4>
 				<p>Je přihlášeno 0 pilotů.</p>
 				<p>
