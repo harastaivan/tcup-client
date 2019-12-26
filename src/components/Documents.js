d1d42689957b4691e1abff934ffec78ed56c7d34
Ivan Harasta <harasta.ivan@gmail.com>
2019-12-26 23:11:56 +0100

    Modify Documents and AddDocument components

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -1,7 +1,78 @@
 import React, { Component } from 'react';
+import { Container, Table, Button, Spinner } from 'reactstrap';
+import { connect } from 'react-redux';
+import Moment from 'react-moment';
+import 'moment/locale/cs';
 
-export default class Documents extends Component {
+import AddDocument from './AddDocument';
+import { getDocuments, deleteDocument, setDocumentsLoading } from '../actions/document';
+import fileSize from '../utils/fileSize';
+
+class Documents extends Component {
+	onDeleteClick = id => {
+		this.props.deleteDocument(id);
+	};
+
+	componentDidMount() {
+		this.props.setDocumentsLoading();
+		this.props.getDocuments();
+	}
+
 	render() {
-		return <div>Dokumenty</div>;
+		const { documents, loading } = this.props.document;
+		const spinner = <Spinner type='grow' color='secondary' className='m-3' />;
+		return (
+			<Container>
+				<h1>Dokumenty</h1>
+				<AddDocument />
+				{loading ? spinner : null}
+				<Table>
+					<thead>
+						<tr>
+							<th>název</th>
+							<th>datum poslední úpravy</th>
+							<th>velikost</th>
+							<th> </th>
+						</tr>
+					</thead>
+					<tbody>
+						{documents.map(document => (
+							<tr key={document._id}>
+								<td>
+									<a href={document.path}>{document.name}</a>
+								</td>
+								<td>
+									<Moment format={'dddd D. M. YYYY HH:mm'} locale='cs'>
+										{document.updatedAt}
+									</Moment>
+								</td>
+								<td>{fileSize(document.size, true)}</td>
+								<td>
+									<Button href={document.path} color='primary' className='mb-1'>
+										download
+									</Button>{' '}
+									{this.props.isAuthenticated && (
+										<Button
+											color='danger'
+											className='mb-1'
+											onClick={this.onDeleteClick.bind(this, document._id)}
+										>
+											smazat
+										</Button>
+									)}
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</Table>
+			</Container>
+		);
 	}
 }
+
+const mapStateToProps = state => ({
+	document: state.document,
+	isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { getDocuments, deleteDocument, setDocumentsLoading })(Documents);
