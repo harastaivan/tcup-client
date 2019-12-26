fc474154e947cc89492b5ef26334d1ae6291902f
Ivan Harasta <harasta.ivan@gmail.com>
2019-12-26 23:10:04 +0100

    Create document reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import errorReducer from './error';
 import authReducer from './auth';
 import newsReducer from './news';
 import registrationReducer from './registration';
+import documentReducer from './document';
 
 export default combineReducers({
-    error: errorReducer,
-    auth: authReducer,
-    news: newsReducer,
-    registration: registrationReducer
+	error: errorReducer,
+	auth: authReducer,
+	news: newsReducer,
+	registration: registrationReducer,
+	document: documentReducer
 });
