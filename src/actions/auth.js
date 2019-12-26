96c062ef5d36f3a7f20bffbaae70688dd1be4e3e
Ivan Harasta <harasta.ivan@gmail.com>
2019-12-26 23:00:25 +0100

    Add optional contentType in tokenConfig

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import {
-    USER_LOADING,
-    USER_LOADED,
-    AUTH_ERROR,
-    REGISTER_SUCCESS,
-    REGISTER_FAIL,
-    LOGOUT_SUCCESS,
-    LOGIN_SUCCESS,
-    LOGIN_FAIL
+  USER_LOADING,
+  USER_LOADED,
+  AUTH_ERROR,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGOUT_SUCCESS,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
 } from './types';
 import { returnErrors } from './error';
 
@@ -15,82 +15,82 @@ const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 
 // Check token & load user
 export const loadUser = () => (dispatch, getState) => {
-    // User loading
-    dispatch({ type: USER_LOADING });
+  // User loading
+  dispatch({ type: USER_LOADING });
 
-    axios
-        .get(`${API_ENDPOINT}/api/auth/user`, tokenConfig(getState))
-        .then(res => dispatch({ type: USER_LOADED, payload: res.data }))
-        .catch(err => {
-            dispatch(returnErrors(err));
-            dispatch({ type: AUTH_ERROR });
-        });
+  axios
+    .get(`${API_ENDPOINT}/api/auth/user`, tokenConfig(getState))
+    .then(res => dispatch({ type: USER_LOADED, payload: res.data }))
+    .catch(err => {
+      dispatch(returnErrors(err));
+      dispatch({ type: AUTH_ERROR });
+    });
 };
 
 // Register User
 export const register = ({ name, surname, email, password }) => dispatch => {
-    // Headers
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
+  // Headers
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
 
-    const body = JSON.stringify({ name, surname, email, password });
+  const body = JSON.stringify({ name, surname, email, password });
 
-    axios
-        .post(`${API_ENDPOINT}/api/users`, body, config)
-        .then(res => dispatch({ type: REGISTER_SUCCESS, payload: res.data }))
-        .catch(err => {
-            dispatch({
-                type: REGISTER_FAIL
-            });
-            dispatch(returnErrors(err, 'REGISTER_FAIL'));
-        });
+  axios
+    .post(`${API_ENDPOINT}/api/users`, body, config)
+    .then(res => dispatch({ type: REGISTER_SUCCESS, payload: res.data }))
+    .catch(err => {
+      dispatch({
+        type: REGISTER_FAIL
+      });
+      dispatch(returnErrors(err, 'REGISTER_FAIL'));
+    });
 };
 
 // Login user
 export const login = ({ email, password }) => dispatch => {
-    // Headers
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
+  // Headers
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
 
-    const body = JSON.stringify({ email, password });
+  const body = JSON.stringify({ email, password });
 
-    axios
-        .post(`${API_ENDPOINT}/api/auth`, body, config)
-        .then(res => dispatch({ type: LOGIN_SUCCESS, payload: res.data }))
-        .catch(err => {
-            dispatch({
-                type: LOGIN_FAIL
-            });
-            dispatch(returnErrors(err, 'LOGIN_FAIL'));
-        });
+  axios
+    .post(`${API_ENDPOINT}/api/auth`, body, config)
+    .then(res => dispatch({ type: LOGIN_SUCCESS, payload: res.data }))
+    .catch(err => {
+      dispatch({
+        type: LOGIN_FAIL
+      });
+      dispatch(returnErrors(err, 'LOGIN_FAIL'));
+    });
 };
 
 // Logout user
 export const logout = () => {
-    return {
-        type: LOGOUT_SUCCESS
-    };
+  return {
+    type: LOGOUT_SUCCESS
+  };
 };
 
 // Setup config/headers and token
-export const tokenConfig = getState => {
-    // Get token from localStorage
-    const token = getState().auth.token;
+export const tokenConfig = (getState, contentType = 'application/json') => {
+  // Get token from localStorage
+  const token = getState().auth.token;
 
-    // Headers
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
+  // Headers
+  const config = {
+    headers: {
+      'Content-Type': contentType
+    }
+  };
 
-    if (token) config.headers['x-auth-token'] = token;
+  if (token) config.headers['x-auth-token'] = token;
 
-    return config;
+  return config;
 };
