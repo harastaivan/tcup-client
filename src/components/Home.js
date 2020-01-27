7ba13eea49733f61274c18d5e76df9c87113c642
Ivan Harasta <harasta.ivan@gmail.com>
2020-01-28 00:09:50 +0100

    Remove REACT_APP_FROM_TO from env variables and change README
    Fixes #65

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
                 </h1>
                 {/*<img src='/img/homepage.jpg' alt='Společná fotka soutěžících' className='homepage'></img>*/}
                 <h2 className="mt-5 mb-5" style={{ fontSize: '3rem', textAlign: 'center' }}>
-                    Letošní tcup se bude konat {process.env.REACT_APP_FROM_TO} v Toužimi.
+                    Letošní tcup se bude konat 11. 7. – 19. 7. 2020 v Toužimi.
                 </h2>
             </div>
         );
