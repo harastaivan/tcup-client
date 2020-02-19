6b5214206a51b4c3520f14429928b8273301d7ce
Ivan Harasta <harasta.ivan@gmail.com>
2020-02-20 00:19:44 +0100

    Translate AddDocument

--- src/components/AddDocument.js
+++ src/components/AddDocument.js
@@ -1,63 +1,45 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Fragment, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, FormGroup, Button, Input, Label } from 'reactstrap';
 
 import { addDocument } from '../actions/document';
+import { useTranslation } from 'react-i18next';
 
-class AddDocument extends React.Component {
-    state = {
-        file: null
-    };
+const AddDocument = () => {
+    const [file, setFile] = useState(null);
 
-    static propTypes = {
-        addDocument: PropTypes.func.isRequired,
-        isAdmin: PropTypes.bool
-    };
+    const document = useSelector((state) => state.document);
+    const isAdmin = useSelector((state) => state.auth.isAdmin);
+    const dispatch = useDispatch();
+    const { t } = useTranslation();
 
-    onChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.files[0]
-        });
-    };
-
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
         const newDocument = {
-            document: this.state.file
+            document: file
         };
-        this.props.addDocument(newDocument);
-        this.setState({
-            file: null
-        });
+        dispatch(addDocument(newDocument));
+        setFile(null);
     };
 
-    render() {
-        const { isAdmin } = this.props;
-        return (
-            <Fragment>
-                {isAdmin ? (
-                    <Fragment>
-                        <h2>Nahrát nový soubor</h2>
-                        <Form onSubmit={this.onSubmit}>
-                            <FormGroup>
-                                <Label for="file">Soubor</Label>
-                                <Input type="file" name="file" id="file" onChange={this.onChange} />
-                            </FormGroup>
-                            <Button color="dark" style={{ marginTop: '2rem' }} disabled={!this.state.file} block>
-                                Nahrát
-                            </Button>
-                        </Form>
-                    </Fragment>
-                ) : null}
-            </Fragment>
-        );
-    }
-}
+    return (
+        <Fragment>
+            {isAdmin ? (
+                <Fragment>
+                    <h2>{t('Nahrát nový soubor')}</h2>
+                    <Form onSubmit={onSubmit}>
+                        <FormGroup>
+                            <Label for="file">Soubor</Label>
+                            <Input type="file" name="file" id="file" onChange={(e) => setFile(e.target.files[0])} />
+                        </FormGroup>
+                        <Button color="dark" style={{ marginTop: '2rem' }} disabled={!file} block>
+                            {t('Nahrát')}
+                        </Button>
+                    </Form>
+                </Fragment>
+            ) : null}
+        </Fragment>
+    );
+};
 
-const mapStateToProps = (state) => ({
-    document: state.document,
-    isAdmin: state.auth.isAdmin
-});
-
-export default connect(mapStateToProps, { addDocument })(AddDocument);
+export default AddDocument;
