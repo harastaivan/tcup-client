8647a58df9ade560cc2f8cefafd38a3fe2090a36
Ivan Harasta <harasta.ivan@gmail.com>
2020-02-19 23:29:51 +0100

    Add i18next config and suspense rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { Spinner } from 'reactstrap';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './i18next';
+
+const loader = <Spinner type="grow" color="secondary" className="m-3" />;
+const root = (
+    <Suspense fallback={loader}>
+        <App />
+    </Suspense>
+);
+
+ReactDOM.render(root, document.getElementById('root'));
