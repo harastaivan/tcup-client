6e138653565dc7fa0a9abe0e8a1898f3d88e2d7c
Ivan Harasta <harasta.ivan@gmail.com>
2020-02-19 23:38:38 +0100

    Convert AppNNavbar to functional component and add SelectLanguage

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { NavLink as Link } from 'react-router-dom';
 import {
     Badge,
@@ -14,151 +14,133 @@ import {
     NavLink,
     UncontrolledDropdown
 } from 'reactstrap';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import SelectLanguage from './SelectLanguage';
 
-class AppNavbar extends Component {
-    state = {
-        open: false
-    };
+const AppNavbar = () => {
+    const [open, setOpen] = useState(false);
+    const { isAuthenticated, isAdmin, user } = useSelector((state) => state.auth);
+    const { t } = useTranslation();
 
-    static propTypes = {
-        auth: PropTypes.object.isRequired
-    };
+    const authLinks = (
+        <Fragment>
+            <UncontrolledDropdown nav inNavbar className="bg-dark mr-4">
+                <DropdownToggle nav caret>
+                    <strong>{user ? `${user.name} ${user.surname}` : ''}</strong>
+                    {isAdmin ? (
+                        <Badge color="danger" className="ml-2">
+                            {t('Admin')}
+                        </Badge>
+                    ) : (
+                        ''
+                    )}
+                </DropdownToggle>
+                <DropdownMenu right className="bg-dark">
+                    <DropdownItem className="bg-dark">
+                        <NavItem>
+                            <NavLink tag={Link} to="/user-profile" activeClassName="active">
+                                {t('Profil')}
+                            </NavLink>
+                        </NavItem>
+                    </DropdownItem>
+                    <DropdownItem className="bg-dark">
+                        <NavItem>
+                            <NavLink tag={Link} to="/change-password" activeClassName="active">
+                                {t('Změnit heslo')}
+                            </NavLink>
+                        </NavItem>
+                    </DropdownItem>
+                </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+                <NavLink tag={Link} to="/logout" activeClassName="active">
+                    {t('Odhlásit se')}
+                </NavLink>
+            </NavItem>
+        </Fragment>
+    );
 
-    toggle = () => {
-        this.setState({
-            open: !this.state.open
-        });
-    };
-
-    render() {
-        const { isAuthenticated, isAdmin, user } = this.props.auth;
-
-        const authLinks = (
-            <Fragment>
-                <UncontrolledDropdown nav inNavbar className="bg-dark mr-4">
-                    <DropdownToggle nav caret>
-                        <strong>{user ? `${user.name} ${user.surname}` : ''}</strong>
-                        {isAdmin ? (
-                            <Badge color="danger" className="ml-2">
-                                Admin
-                            </Badge>
-                        ) : (
-                            ''
-                        )}
-                    </DropdownToggle>
-                    <DropdownMenu right className="bg-dark">
-                        <DropdownItem className="bg-dark">
-                            <NavItem>
-                                <NavLink tag={Link} to="/user-profile" activeClassName="active">
-                                    Profil
-                                </NavLink>
-                            </NavItem>
-                        </DropdownItem>
-                        <DropdownItem className="bg-dark">
-                            <NavItem>
-                                <NavLink tag={Link} to="/change-password" activeClassName="active">
-                                    Změnit heslo
-                                </NavLink>
-                            </NavItem>
-                        </DropdownItem>
-                    </DropdownMenu>
-                </UncontrolledDropdown>
-                <NavItem>
-                    <NavLink tag={Link} to="/logout" activeClassName="active">
-                        Odhlásit se
-                    </NavLink>
-                </NavItem>
-            </Fragment>
-        );
-
-        const guestLinks = (
-            <Fragment>
-                <NavItem>
-                    <NavLink tag={Link} to="/login" activeClassName="active">
-                        Přihlásit se
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink tag={Link} to="/signup" activeClassName="active">
-                        Registrace
-                    </NavLink>
-                </NavItem>
-            </Fragment>
-        );
-        return (
-            <Fragment>
-                <Navbar color="dark" dark expand="lg">
-                    <NavbarBrand tag={Link} to="/">
-                        {process.env.REACT_APP_TITLE}
-                    </NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.open} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink tag={Link} to="/news" activeClassName="active">
-                                    Novinky
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to="/statuses" activeClassName="active" disabled>
-                                    Statusy soutěžících
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to="/starting-list" activeClassName="active">
-                                    Startovní listina
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to="/results" activeClassName="active" disabled>
-                                    Úlohy a výsledky
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} to="/registration" activeClassName="active">
-                                    Přihláška
-                                </NavLink>
-                            </NavItem>
-                            <NavItem />
-                            <NavItem>
-                                <NavLink tag={Link} to="/igc" activeClassName="active" disabled>
-                                    Odeslat IGC
-                                </NavLink>
-                            </NavItem>
-                            <UncontrolledDropdown nav inNavbar className="bg-dark mr-4">
-                                <DropdownToggle nav caret>
-                                    Další
-                                </DropdownToggle>
-                                <DropdownMenu right className="bg-dark">
-                                    <DropdownItem className="bg-dark">
-                                        <NavLink tag={Link} to="/documents" activeClassName="active">
-                                            Dokumenty
+    const guestLinks = (
+        <Fragment>
+            <NavItem>
+                <NavLink tag={Link} to="/login" activeClassName="active">
+                    {t('Přihlásit se')}
+                </NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink tag={Link} to="/signup" activeClassName="active">
+                    {t('Registrace')}
+                </NavLink>
+            </NavItem>
+        </Fragment>
+    );
+    return (
+        <Fragment>
+            <Navbar color="dark" dark expand="lg">
+                <NavbarBrand tag={Link} to="/">
+                    {process.env.REACT_APP_TITLE}
+                </NavbarBrand>
+                <NavbarToggler onClick={() => setOpen(!open)} />
+                <Collapse isOpen={open} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink tag={Link} to="/news" activeClassName="active">
+                                {t('Novinky')}
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/statuses" activeClassName="active" disabled>
+                                {t('Statusy soutěžících')}
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/starting-list" activeClassName="active">
+                                {t('Startovní listina')}
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/results" activeClassName="active" disabled>
+                                {t('Úlohy a výsledky')}
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/registration" activeClassName="active">
+                                {t('Přihláška')}
+                            </NavLink>
+                        </NavItem>
+                        <NavItem />
+                        <NavItem>
+                            <NavLink tag={Link} to="/igc" activeClassName="active" disabled>
+                                {t('Odeslat IGC')}
+                            </NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar className="bg-dark mr-4">
+                            <DropdownToggle nav caret>
+                                {t('Další')}
+                            </DropdownToggle>
+                            <DropdownMenu right className="bg-dark">
+                                <DropdownItem className="bg-dark">
+                                    <NavLink tag={Link} to="/documents" activeClassName="active">
+                                        {t('Dokumenty')}
+                                    </NavLink>
+                                </DropdownItem>
+                                <DropdownItem className="bg-dark">
+                                    <NavItem>
+                                        <NavLink tag={Link} to="/contacts" activeClassName="active">
+                                            {t('Kontakty')}
                                         </NavLink>
-                                    </DropdownItem>
-                                    <DropdownItem className="bg-dark">
-                                        <NavItem>
-                                            <NavLink tag={Link} to="/contacts" activeClassName="active">
-                                                Kontakty
-                                            </NavLink>
-                                        </NavItem>
-                                    </DropdownItem>
-                                </DropdownMenu>
-                            </UncontrolledDropdown>
-                            {isAuthenticated ? authLinks : guestLinks}
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </Fragment>
-        );
-    }
-}
-
-const mapStateToProps = (state) => {
-    return {
-        auth: state.auth
-    };
+                                    </NavItem>
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                        {isAuthenticated ? authLinks : guestLinks}
+                        <SelectLanguage />
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </Fragment>
+    );
 };
 
-export default connect(mapStateToProps, null)(AppNavbar);
+export default AppNavbar;
