f0c16f2785301136714814863d18bf39cf436bf1
Ivan Harasta <harasta.ivan@gmail.com>
2020-02-20 01:04:25 +0100

    Translate Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,29 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router';
+import React from 'react';
 import { Container } from 'reactstrap';
+import { useTranslation } from 'react-i18next';
+
 import { version } from '../../package.json';
 
-class Footer extends Component {
-    render() {
-        // const isHomePage = this.props.history.location.pathname === '/';
-        return (
-            <Container className="transparent-background">
-                <footer className={`p-5 py-4 text-center text-small`}>
-                    <p>
-                        © {process.env.REACT_APP_TITLE} <b>verze {version}</b>
-                    </p>
-                    <p>
-                        by{' '}
-                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/harastaivan">
-                            @harastaivan
-                        </a>
-                    </p>
-                </footer>
-            </Container>
-        );
-    }
-}
+const Footer = () => {
+    const { t } = useTranslation();
+    return (
+        <Container className="transparent-background">
+            <footer className={`p-5 py-4 text-center text-small`}>
+                <p>
+                    © {process.env.REACT_APP_TITLE}{' '}
+                    <b>
+                        {t('verze')} {version}
+                    </b>
+                </p>
+                <p>
+                    by{' '}
+                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/harastaivan">
+                        @harastaivan
+                    </a>
+                </p>
+            </footer>
+        </Container>
+    );
+};
 
-export default withRouter(Footer);
+export default Footer;
