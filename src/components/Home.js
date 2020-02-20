a2fa8b2be2dfcc5b3035fef562377cd3ac69417c
Ivan Harasta <harasta.ivan@gmail.com>
2020-02-20 01:14:35 +0100

    Translate Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
-
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1 className="mx-auto" style={{ fontSize: '6rem', textAlign: 'center' }}>
-                    {process.env.REACT_APP_TITLE}
-                </h1>
-                {/*<img src='/img/homepage.jpg' alt='Společná fotka soutěžících' className='homepage'></img>*/}
-                <h2 className="mt-5 mb-5" style={{ fontSize: '3rem', textAlign: 'center' }}>
-                    Letošní tcup se bude konat <br />
-                    11. 7. – 19. 7. 2020 v Toužimi.
-                </h2>
-            </div>
-        );
-    }
-}
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+const Home = () => {
+    const { t } = useTranslation();
+    return (
+        <div>
+            <h1 className="mx-auto" style={{ fontSize: '6rem', textAlign: 'center' }}>
+                {process.env.REACT_APP_TITLE}
+            </h1>
+            <h2 className="mt-5 mb-5" style={{ fontSize: '3rem', textAlign: 'center' }}>
+                {t('Letošní tcup se bude konat')} <br />
+                {t('11. 7. – 19. 7. 2020 v Toužimi')}.
+            </h2>
+        </div>
+    );
+};
 
 export default Home;
