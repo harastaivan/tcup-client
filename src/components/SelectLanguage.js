c2fe2da8966dd78002d225601d29e54ac108c412
Ivan Harasta <harasta.ivan@gmail.com>
2020-03-06 17:28:53 +0100

    Change icon from emoji to svg cdn

--- src/components/SelectLanguage.js
+++ src/components/SelectLanguage.js
@@ -2,8 +2,29 @@ import React from 'react';
 import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavLink, NavItem } from 'reactstrap';
 import ReactCountryFlag from 'react-country-flag';
 import { useTranslation } from 'react-i18next';
+import PropTypes from 'prop-types';
 
 import getCountryCode from '../utils/countryCode';
+
+const Flag = (props) => (
+    <ReactCountryFlag
+        countryCode={props.language}
+        svg
+        cdnUrl="https://cdn.staticaly.com/gh/hjnilsson/country-flags/master/svg/"
+        cdnSuffix="svg"
+        style={{
+            height: 'auto',
+            width: '1.5em',
+            border: '1px solid #222',
+            borderRadius: '2px',
+            objectFit: 'cover'
+        }}
+    />
+);
+
+Flag.propTypes = {
+    language: PropTypes.string.isRequired
+};
 
 export default function SelectLanguage() {
     const { i18n } = useTranslation();
@@ -11,18 +32,18 @@ export default function SelectLanguage() {
     return (
         <UncontrolledDropdown nav inNavbar className="bg-dark mr-4">
             <DropdownToggle nav caret>
-                <ReactCountryFlag countryCode={getCountryCode(i18n.language)} />
+                <Flag language={getCountryCode(i18n.language)} />
             </DropdownToggle>
             <DropdownMenu right className="bg-dark">
                 <DropdownItem className="bg-dark">
                     <NavLink onClick={() => i18n.changeLanguage('cs')}>
-                        <ReactCountryFlag countryCode="cz" /> česky
+                        <Flag language={'cz'} /> česky
                     </NavLink>
                 </DropdownItem>
                 <DropdownItem className="bg-dark">
                     <NavItem>
                         <NavLink onClick={() => i18n.changeLanguage('en')}>
-                            <ReactCountryFlag countryCode="gb" /> english
+                            <Flag language={'gb'} /> english
                         </NavLink>
                     </NavItem>
                 </DropdownItem>
