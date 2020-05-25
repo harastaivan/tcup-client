64e1ee3eba5961d6cc7b4d787f52244a19b50517
Ivan Harasta <harasta.ivan@gmail.com>
2020-05-25 22:09:23 +0200

    Redesign homepage with big showcase picture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, useRouteMatch } from 'react-router-dom';
 import { Provider, useSelector, useDispatch } from 'react-redux';
 import { Alert, Container } from 'reactstrap';
 
@@ -40,46 +40,51 @@ const App = () => {
 
     const { t } = useTranslation();
 
+    const match = useRouteMatch('/');
+    const isHomepage = match && match.isExact;
+
     return (
-        <Router>
-            <div className="App">
-                <AppNavbar />
-                <Container className="p-3 min-vh-container transparent-background">
-                    {process.env.REACT_APP_TEST_MODE && (
-                        <Alert color="warning">
-                            {t('Aplikace se právě testuje. Nemusí vše fungovat správně.')}{' '}
-                            <a href="https://gitreports.com/issue/harastaivan/tcup-client">{t('Chyby hlašte zde')}</a>.
-                        </Alert>
-                    )}
-                    {error.msg && <Alert color="danger">{t(error.msg)}</Alert>}
-                    <Switch>
-                        <Route path="/" component={Home} exact />
-                        <Route path="/news" component={News} />
-                        <Route path="/registration" component={Registration} />
-                        <Route path="/login" component={Login} />
-                        <Route path="/signup" component={Signup} />
-                        <Route path="/logout" component={Logout} />
-                        <Route path="/statuses" component={Statuses} />
-                        <Route path="/starting-list" component={StartingList} />
-                        <Route path="/results" component={Results} />
-                        <Route path="/documents" component={Documents} />
-                        <Route path="/contacts" component={Contacts} />
-                        <Route path="/igc" component={SendIgc} />
-                        <Route path="/user-profile" component={UserSettings} />
-                        <Route path="/change-password" component={ChangePassword} />
-                        <Route path="/archive" component={Archive} />
-                    </Switch>
-                </Container>
-                <Footer />
-            </div>
-        </Router>
+        <div className="App">
+            <AppNavbar />
+            <Switch>
+                <Route path="/" component={Home} exact />
+            </Switch>
+            <Container className={!isHomepage && 'p-3 min-vh-container transparent-background'}>
+                {process.env.REACT_APP_TEST_MODE === 'true' && (
+                    <Alert color="warning">
+                        {t('Aplikace se právě testuje. Nemusí vše fungovat správně.')}{' '}
+                        <a href="https://gitreports.com/issue/harastaivan/tcup-client">{t('Chyby hlašte zde')}</a>.
+                    </Alert>
+                )}
+                {error.msg && <Alert color="danger">{t(error.msg)}</Alert>}
+                <Switch>
+                    <Route path="/news" component={News} />
+                    <Route path="/registration" component={Registration} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/logout" component={Logout} />
+                    <Route path="/statuses" component={Statuses} />
+                    <Route path="/starting-list" component={StartingList} />
+                    <Route path="/results" component={Results} />
+                    <Route path="/documents" component={Documents} />
+                    <Route path="/contacts" component={Contacts} />
+                    <Route path="/igc" component={SendIgc} />
+                    <Route path="/user-profile" component={UserSettings} />
+                    <Route path="/change-password" component={ChangePassword} />
+                    <Route path="/archive" component={Archive} />
+                </Switch>
+            </Container>
+            {!isHomepage && <Footer />}
+        </div>
     );
 };
 
 const WrappedApp = () => {
     return (
         <Provider store={store}>
-            <App />
+            <Router>
+                <App />
+            </Router>
         </Provider>
     );
 };
