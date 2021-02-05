55d959c4ee55752a346729845742001217c31661
Ivan Harasta <harasta.ivan@gmail.com>
2021-02-05 22:19:25 +0100

    Fix store

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import rootReducer from './store/rootReducer'
 
 const initialState = {}
@@ -9,9 +11,9 @@ const middleware = [thunk]
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    composeWithDevTools(
+        applyMiddleware(...middleware)
+        // Other store enhancers if any
     )
 )
 
