7b6e50c42172086f74aff43107fdb1b370cebe92
Ivan Harasta <harasta.ivan@gmail.com>
2021-02-21 23:47:11 +0100

    Enable registrations

--- src/containers/AppNavbar.tsx
+++ src/containers/AppNavbar.tsx
@@ -108,7 +108,7 @@ const AppNavbar = () => {
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink tag={Link} to="/registration" activeClassName="active" exact disabled>
+                            <NavLink tag={Link} to="/registration" activeClassName="active" exact>
                                 {t('Přihláška')}
                             </NavLink>
                         </NavItem>
