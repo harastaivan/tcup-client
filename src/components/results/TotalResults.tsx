711f238cdf3655248bb4fb817e9326c271f9ed41
Ivan Harasta <harasta.ivan@gmail.com>
2021-04-17 23:24:57 +0200

    Implement TotalResults

---
+++ src/components/results/TotalResults.tsx
@@ -0,0 +1,76 @@
+import { useTranslation } from 'react-i18next'
+import { Button, Col, Row, Table } from 'reactstrap'
+
+import ResultsTitle from './ResultsTitle'
+import Spinner from '../Spinner'
+import { useTotalResults } from '../../hooks/useResults'
+
+type Props = {
+    competitionClass: { id: string; name: string }
+    goToTopResults: () => void
+    goToDailyResults: (competitionClass: { id: string; name: string }) => () => void
+}
+
+const TotalResults = ({ competitionClass, goToTopResults, goToDailyResults }: Props) => {
+    const { t } = useTranslation()
+
+    const { results, loaded, error } = useTotalResults(competitionClass.id)
+
+    return (
+        <div style={{ marginTop: '2rem' }}>
+            <ResultsTitle>{`${t('Celkové výsledky')} (${competitionClass.name})`}</ResultsTitle>
+            {error && t('Došlo k chybě')}
+            {!loaded && !error && (
+                <div style={{ minHeight: '60vh' }}>
+                    <Spinner />
+                </div>
+            )}
+            {loaded && results && (
+                <Table striped responsive style={{ marginTop: '2rem' }}>
+                    <thead>
+                        <tr>
+                            <th>{t('#')}</th>
+                            <th>&Delta;</th>
+                            <th>{t('start. č.')}</th>
+                            <th>{t('jméno')}</th>
+                            <th>{t('aeroklub')}</th>
+                            <th>{t('typ')}</th>
+                            <th>{t('body')}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {results.map((result, index) => (
+                            <tr key={index}>
+                                <td>{result.position}</td>
+                                <td>{result.gainedPosition}</td>
+                                <td>{result.startNumber}</td>
+                                <td>{result.name}</td>
+                                <td>{result.aeroclub}</td>
+                                <td>{`${result.glider} (${result.handicap})`}</td>
+                                <td>{result.points}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )}
+            <Row style={{ marginTop: '2rem' }}>
+                <Col md="6">
+                    <Button style={{ width: '100%', marginBottom: '10px' }} color="secondary" onClick={goToTopResults}>
+                        Zpět
+                    </Button>
+                </Col>
+                <Col md="6">
+                    <Button
+                        style={{ width: '100%', marginBottom: '10px' }}
+                        color="secondary"
+                        outline
+                        onClick={goToDailyResults(competitionClass)}>
+                        Denní výsledky
+                    </Button>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default TotalResults
