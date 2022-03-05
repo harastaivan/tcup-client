495fb55ea7925d849388e214aaa1a13509ac9dbd
Ivan Harasta <ivan.harasta@ackee.cz>
2022-03-05 11:59:42 +0100

    ✨ Introduce SpinnerFullPage

---
+++ src/components/SpinnerFullPage.tsx
@@ -0,0 +1,20 @@
+import { APP_TITLE } from '../constants'
+import Spinner from './Spinner'
+
+const SpinnerFullPage = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                height: '100vh',
+            }}>
+            <h1 style={{ fontSize: '5rem', marginBottom: '1rem', textAlign: 'center' }}>{APP_TITLE}</h1>
+            <Spinner />
+        </div>
+    )
+}
+
+export default SpinnerFullPage
