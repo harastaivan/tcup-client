a61766637e118d40b152f43dfea779828611a182
Ivan Harasta <ivan.harasta@ackee.cz>
2022-03-30 17:18:55 +0200

    🔥 Remove REACT_APP_TITLE from env vars

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,9 @@
 export const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || ''
 export const TEST_MODE = process.env.REACT_APP_TEST_MODE === 'true'
 export const SOARING_SPOT_URL = process.env.REACT_APP_SOARING_SPOT_URL
-export const APP_TITLE = process.env.REACT_APP_TITLE
 export const SIGNUP_DISABLED =
     process.env.REACT_APP_SIGNUP_DISABLED !== undefined ? JSON.parse(process.env.REACT_APP_SIGNUP_DISABLED) : true
 export const SPINNER_TIMEOUT =
     process.env.REACT_APP_SPINNER_TIMEOUT !== undefined ? Number(process.env.REACT_APP_SPINNER_TIMEOUT) : 2000
+
+export const APP_TITLE = 'tcup 2022'
