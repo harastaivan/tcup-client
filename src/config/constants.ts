3815235813c913be0b18089fa8c88bed49d91501
Ivan Harasta <ivan.harasta@ackee.cz>
2022-03-30 17:46:48 +0200

    🐛 Add app title back

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,4 +6,4 @@ export const SIGNUP_DISABLED =
 export const SPINNER_TIMEOUT =
     process.env.REACT_APP_SPINNER_TIMEOUT !== undefined ? Number(process.env.REACT_APP_SPINNER_TIMEOUT) : 2000
 
-export const APP_TITLE = 'tcup 2022'
+export const APP_TITLE = process.env.REACT_APP_TITLE
