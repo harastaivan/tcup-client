2b26c63865c7bf557502313132f5bc58e13a7551
Ivan Harasta <ivan.harasta@ackee.cz>
2022-07-07 09:50:11 +0200

    🔧 Update soaring spot url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || ''
 export const TEST_MODE = process.env.REACT_APP_TEST_MODE === 'true'
-export const SOARING_SPOT_URL = process.env.REACT_APP_SOARING_SPOT_URL
+export const SOARING_SPOT_URL = 'https://www.soaringspot.com/en_gb/t-cup-2022-touzim-2022/results'
 export const SPINNER_TIMEOUT =
     process.env.REACT_APP_SPINNER_TIMEOUT !== undefined ? Number(process.env.REACT_APP_SPINNER_TIMEOUT) : 2000
 
